6afe8f4cb5692d8c46c484b2df24e9cc9e0a8579
keikai713 <alejandro.duran.1730@gmail.com>
2023-06-22 14:48:06 -0600

    The project is added

---
+++ src/juegoadivinar/JuegoAdivinar.java
@@ -0,0 +1,13 @@
+package juegoadivinar;
+
+/* Autor: Alejandro Pérez Durán.
+ * Fecha de creación: 30 de Enero de 2015.
+ * Descripción del programa: Realizar un juego donde se adivine un número.
+ */
+public class JuegoAdivinar {
+
+    public static void main(String[] args) {
+        Ventana EM = new Ventana();
+        EM.setVisible(true);
+    }
+}
